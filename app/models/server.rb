d126c3c93323f7056b45ec48a9f6ee88253aabc3
regisgregol <regis.gregol@itai.org.br>
2013-03-18 17:34:43 -0300

    Fazer em casa euhuheuhae tchau

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,5 +2,5 @@ class Server < ActiveRecord::Base
   belongs_to :device
   belongs_to :type
   belongs_to :service
-  attr_accessible :ip, :name, :password, :port, :root_password, :sysadm, :user
+  attr_accessible :ip, :name, :password, :port, :root_password, :sysadm, :user, :device_id, :type_id, :service_id
 end
